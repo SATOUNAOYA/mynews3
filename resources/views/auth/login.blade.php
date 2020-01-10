3bb364b249eef136655e3867946475760e3dd98d
”sato” <naoya120328@icloud.com>
2020-01-10 16:48:17 +0000

    laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="login-box card">
-                    <div class="login-header card-header mx-auto">{{ _(‘messages.messages.Login‘) }}</div>
+                    <div class="login-header card-header mx-auto">{{ _('messages.Login') }}</div>
 
                     <div class="login-body card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">{{ _(‘messages.E-MailAddress‘) }}</label>
+                                <label for="email" class="col-sm-4 col-form-label text-md-right">{{ _('messages.E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +26,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ _(‘messages.Password‘) }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ _('messages.Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -43,7 +43,7 @@
                                 <div class="col-md-6 offset-md-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ _(‘messages.RememberMe‘) }}
+                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ _('messages.Remember　Me') }}
                                         </label>
                                     </div>
                                 </div>
