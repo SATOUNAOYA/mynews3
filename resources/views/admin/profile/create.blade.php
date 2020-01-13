6fa5379b3c2a7109cd29ded6aee62b25ad25023c
”sato” <naoya120328@icloud.com>
2020-01-13 11:59:51 +0000

    laravel

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,6 +1,6 @@
 {{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
-@section('title', 'My profile')
+@section('title', '自己紹介')
 
 @section('content')
     <div class="container">
@@ -16,22 +16,38 @@
                             @endforeach
                         </ul>
                     @endif
-                    <body>
-<form action="#" method="post">
-	<p>お名前（必須）：<br>
-	<input type="text" name="name"></p>
-	<p>性別：<br>
-	<input type="text" name="gender"></p>
-    <p>趣味</p>
-    <input type="text" name="hobby"/>
-</form>
-</body>
-                    
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
+                            <input type="text" class="form-control" name="introduction" value="{{ old('introduction') }}">
                         </div>
                     </div>
-                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
+
+                   
+                  
+                    
