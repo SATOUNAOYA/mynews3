d34c0fa1ca545173d09fb46a3dd453efc891fe16
”sato” <naoya120328@icloud.com>
2020-01-17 06:32:32 +0000

    laravel

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+});
 
 
 
